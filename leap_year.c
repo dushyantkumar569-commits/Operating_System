6d618de09a481a16c79df9128c066f5b79e13393
Dushyant Kumar <dushyantyadav569@gmail.com>
2026-02-07 15:51:23 +0530

    Update leap_year.c

--- leap_year.c
+++ leap_year.c
@@ -3,7 +3,7 @@
 int main()
 {
     int year;
-    printf("Enter  leap year:");
+    printf("Enter a leap year:");
     scanf("%d", &year);
     
     if((year % 4 == 0 && year % 100 != 0 ) || (year % 4 == 0)){
