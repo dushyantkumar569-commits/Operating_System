c1f7f4844432f5edbf7a61b9483229730f45fa2d
Dushyant Kumar <dushyantyadav569@gmail.com>
2026-02-07 15:44:41 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -9,7 +9,7 @@ int main() {
     if (num <= 1) {
         isPrime = 0;
     } else {
-        for (i = 2; i < num/2; i++) {
+        for (i = 2; i < num \ 2; i++) {
             if (num % i == 0) {
                 isPrime = 0;
                 break;
